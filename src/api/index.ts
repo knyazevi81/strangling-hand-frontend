9d215ab876db44cd264e0c260623fa1158de0d8d
Your Name <you@example.com>
2026-04-09 01:10:54 +0000

    t

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_BASE_URL = 'http://localhost:8000'
+export const API_BASE_URL = 'https://api.savebit.ru'
 
 const api = axios.create({
   baseURL: `${API_BASE_URL}/api/v1`,
